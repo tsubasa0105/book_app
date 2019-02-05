5a223b88884da26befcc7ab4b1e2b355bd2b14a0
tsubasaishikawa <t.ishikawa0105@gmail.com>
2019-02-05 18:45:45 +0900

    Add validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,7 @@
 class Book < ApplicationRecord
   mount_uploader :picture,PictureUploader
+  validates :title, presence: true, uniqueness: true,length: { maximum: 30 }
+  validates :memo, presence: true, length: { maximum: 400 }
+  validates :author, presence: true, length: { maximum: 20 }
+  validates_size_of :picture, maximum: 2.megabytes, message: "2MBを超えています。"
 end
