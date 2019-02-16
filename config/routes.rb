9a6bc35ae22bfbb07ae49cff1e5af671ca6c3e4d
tsubasaishikawa <t.ishikawa0105@gmail.com>
2019-02-16 12:52:12 +0900

    Set up omniauth to devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
   root 'books#index'
   scope "(:locale)" do
 	  resources :books
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
