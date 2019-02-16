d87d051d5afdbbcbe505d8db2573acaa47ddec42
tsubasaishikawa <t.ishikawa0105@gmail.com>
2019-02-16 13:21:54 +0900

    Remove unnecessary and Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    omniauth_callbacks: 'users/omniauth_callbacks',
-    registrations: 'users/registrations'
-  }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'books#index'
   scope "(:locale)" do
 	  resources :books
