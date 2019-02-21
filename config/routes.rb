1337f50d67d975592706d53a0e16a58f13ede583
tsubasaishikawa <t.ishikawa0105@gmail.com>
2019-02-21 21:48:50 +0900

    Set up active storage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  root 'books#index'
+  devise_for :users, only: :omniauth_callbacks, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
+
   scope "(:locale)" do
-	  resources :books
+    resources :books
+    root 'books#index'
+    devise_for :users, skip: :omniauth_callbacks, controllers: {registrations: 'users/registrations'}
   end
 end
