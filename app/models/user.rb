54451aaae7a7d5a333ef9b32e8ae7a1b99b5afef
tsubasaishikawa <t.ishikawa0105@gmail.com>
2019-02-27 23:10:53 +0900

    Add setting for active storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:facebook]
+  has_one_attached :avatar
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
