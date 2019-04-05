7e19c2760aad62dc125e2f8dc1445ba18f68db1b
tsubasaishikawa <t.ishikawa0105@gmail.com>
2019-04-05 14:52:53 +0900

    Fix model spec because validation tests weren't needed but method tests that I inplemented needed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,36 +3,50 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  it "is valid with an email address and password" do
-    user = User.new(
-      email: "testtest@example.com",
-      password: "password",
-    )
-    expect(user).to be_valid
+  describe "#from_omniauth" do
+    it "finds an existing user by the provider and uid fields" do
+      user = User.create(
+        provider: "facebook",
+        uid: 283283,
+        email: "testtest@example.com",
+        password: "password",
+        password_confirmation: "password"
+          )
+
+      omniauth_user = User.where(provider: "facebook", uid: 283283).first
+      expect(omniauth_user.email).to eq("testtest@example.com")
+    end
   end
 
-  it "is valid without an email address" do
-    user = User.new(email: nil)
-    user.valid?
-    expect(user.errors[:email]).to include("を入力してください")
-  end
 
-  it "is valid without a password" do
-    user = User.new(password: nil)
-    user.valid?
-    expect(user.errors[:password]).to include("を入力してください")
-  end
+  describe "#new_with_session" do
+    it "fills a email data from sessoin if user email is blank" do
+      session = {
+        facebook_data: {
+        provider: "facebook",
+        uid:  "283283",
+        name: "testuser",
+        email: "testuser@example.com",
+        credentials: {
+          token: "testtoken"
+        },
+        extra: {
+          raw_info: "raw_info"
+        }
+        }
+      }
+      data = session
 
-  it "is invalid with a duplicate email address" do
-    User.create(
-      email: "testtest@example.com",
-      password: "password",
-    )
-    user = User.new(
-      email: "testtest@example.com",
-      password: "password",
-    )
-    user.valid?
-    expect(user.errors[:email]).to include("はすでに存在します")
+      user = User.new(
+        provider: "facebook",
+        uid: 283283,
+        email: "",
+        password: "password",
+        password_confirmation: "password"
+      )
+
+      user.email = data[:facebook_data][:email] if user.email.blank?
+      expect(user.email).to eq(data[:facebook_data][:email])
+    end
   end
 end
