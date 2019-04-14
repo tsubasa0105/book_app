60e4ad01a74fa4a6e2b15bf9c31d6a2499a89981
tsubasaishikawa <t.ishikawa0105@gmail.com>
2019-04-14 23:21:37 +0900

    Add method test in model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,14 @@ require "rails_helper"
 RSpec.describe User, type: :model do
   describe "#from_omniauth" do
     it "finds an existing user by the provider and uid fields" do
+      auth = OmniAuth::AuthHash.new(
+        provider: "facebook",
+        uid: "283283",
+        info: {
+          email: "testtest@example.com",
+          name: "testuser"
+        }
+      )
       user = User.create(
         provider: "facebook",
         uid: 283283,
@@ -12,8 +20,7 @@ RSpec.describe User, type: :model do
         password: "password",
         password_confirmation: "password"
           )
-
-      omniauth_user = User.where(provider: "facebook", uid: 283283).first
+      omniauth_user = User.from_omniauth(auth)
       expect(omniauth_user.email).to eq("testtest@example.com")
     end
   end
@@ -22,31 +29,24 @@ RSpec.describe User, type: :model do
   describe "#new_with_session" do
     it "fills a email data from sessoin if user email is blank" do
       session = {
-        facebook_data: {
-        provider: "facebook",
-        uid:  "283283",
-        name: "testuser",
-        email: "testuser@example.com",
-        credentials: {
-          token: "testtoken"
+        "devise.facebook_data" => {
+        "provider" => "facebook",
+        "uid" => "283283",
+        "name" => "testuser",
+        "email" => "testuser@example.com",
+        "credentials" => {
+          "token" => "testtoken"
         },
-        extra: {
-          raw_info: "raw_info"
+        "extra" => {
+          "raw_info" => "raw_info"
         }
         }
       }
       data = session
 
-      user = User.new(
-        provider: "facebook",
-        uid: 283283,
-        email: "",
-        password: "password",
-        password_confirmation: "password"
-      )
-
-      user.email = data[:facebook_data][:email] if user.email.blank?
-      expect(user.email).to eq(data[:facebook_data][:email])
+      params = {}
+      user = User.new_with_session(params, session)
+      expect(user.email).to eq(data["devise.facebook_data"]["email"])
     end
   end
 end
